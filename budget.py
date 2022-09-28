9939ac50d39b9b26157bc0fee4c223c997b7d43e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2022-09-28 22:00:57 +0000

    feat: implemented custom __str__ method for Category class
    fix: updated transfer method to return true or false and fixed typo in transfer deposit description.

--- budget.py
+++ budget.py
@@ -37,7 +37,55 @@ class Category:
 
     if has_funds_available:
       self.withdraw(amount, f'Transfer to {category.name}')
-      category.deposit(amount, f'Transfer to {self.name}')
+      category.deposit(amount, f'Transfer from {self.name}')
+      return True
+
+    return False
+
+  def __str__(self):
+    lines_of_text = []
+    
+    name_len = len(self.name)
+    num_of_asterisks = 30 - name_len
+    left_asterisks = int(num_of_asterisks / 2)
+    right_asterisks = num_of_asterisks - left_asterisks
+    
+    title = ("*" * left_asterisks) + self.name + ("*" * right_asterisks)
+    lines_of_text.append(title)
+    
+    for item in self.ledger:
+      item_text = ""
+      
+      description = item["description"]
+      description_len = len(description)
+      
+      if description_len > 23:
+        description = description[:23]
+
+      description_len = len(description)
+      
+      item_text += description
+
+      amount = str(item["amount"])
+      
+      if "." not in amount:
+        amount += ".00"
+
+      amount_len = len(amount)
+
+      num_of_spaces = 30 - (description_len + amount_len)
+
+      amount = (" " * num_of_spaces) + amount
+      
+      item_text += amount
+      
+      lines_of_text.append(item_text)
+
+    balance = str(self.get_balance())
+    total_text = f'Total: {balance}' 
+    lines_of_text.append(total_text)
+    
+    return "\n".join(lines_of_text)
 
 def create_spend_chart(categories):
   print("TODO: implement creating a spend chart")
